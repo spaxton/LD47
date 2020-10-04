dccef4e96791d5988d4ae05383f97a5afc913d30
SethPaxton <spaxton0492@gmail.com>
2020-10-03 19:39:43 -0700

    Let there be gravity!
    Platformer basics 101

--- LD47_Graveyard/Assets/Scripts/scr_player_movement.cs
+++ LD47_Graveyard/Assets/Scripts/scr_player_movement.cs
@@ -7,11 +7,19 @@ public class scr_player_movement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float walkSpeed = 4f;
+    public float walkSpeed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public float turnSmoothing = 0.1f;
     float turnSmoothVelocity;
-    
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 vel;
+    bool onGround;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +30,21 @@ public class scr_player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // are you on the ground? Act like it
+        onGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(onGround && vel.y <0)
+        {
+            vel.y = -2f;
+        }
+
+        // Is the player trying to move?
         float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
         Vector3 dir = new Vector3(hor, 0f, ver).normalized;
 
+        
+        // if you're moving, move, and look like you're moving
         if (dir.magnitude >= 0.1f)
         {
             float tarAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -35,5 +54,15 @@ public class scr_player_movement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, tarAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * walkSpeed * Time.deltaTime);
         }
+
+        if(Input.GetButtonDown("Jump") && onGround)
+        {
+            vel.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // under pressure (gravitational pressure, that is)
+        vel.y += gravity * Time.deltaTime;
+
+        controller.Move(vel * Time.deltaTime);
     }
 }
